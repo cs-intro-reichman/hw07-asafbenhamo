3ed3a9af02106d27c47a400219868cda51aa33b9
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2026-01-01 20:44:42 +0200

    jjj

--- Binomial.java
+++ Binomial.java
@@ -8,23 +8,20 @@ public class Binomial {
 
 		// Testing the optimized binomial implementation:
 		// System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
-		int n = Integer.parseInt(args[0]);
-        int k = Integer.parseInt(args[1]);
-        System.out.println(binomial(n, k));
-        System.out.println(binomial1(n, k));
+		
 	}
 
 	// Computes the Binomial function, basic version.
-	public static int binomial1(int n, int k) { 
+	public static long binomial1(int n, int k) { 
 		if (k > n) return 0;
         if (k == 0 || n == 0) return 1;
         return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		if (k > n) return 0;
-        int[][] memo = new int[n + 1][k + 1];
+        long[][] memo = new long[n + 1][k + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= k; j++) {
                 memo[i][j] = -1;
@@ -33,7 +30,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
